f550d8957b9579c6bfb57244b82adc5836f6adab
odrianoaliveira <odrianoaliveira@user.noreply.github.com>
2024-07-06 17:16:35 +0200

    wip: get value hadler

--- keyvaluestore/keyvalue/getvalue_handler.go
+++ keyvaluestore/keyvalue/getvalue_handler.go
@@ -1,13 +1,32 @@
 package keyvalue
 
-//func GetValueByKey(w http.ResponseWriter, r *http.Request) {
-//	if r.Method != http.MethodGet {
-//		http.Error(w, "Invalid Method Request", http.StatusMethodNotAllowed)
-//		return
-//	}
-//
-//	key := r.PathValue("key")
-//
-//	writer := http.ResponseWriter(w, "Key: %"+key+" Value: %s"+value)
-//
-//}
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type KeyValueResponse struct {
+	Key   string
+	Value interface{}
+}
+
+func GetValueByKey(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid Method Request", http.StatusMethodNotAllowed)
+		return
+	}
+
+	key := r.PathValue("key")
+
+	response := KeyValueResponse{
+		Key:   key,
+		Value: "a value", // TODO reade from the hash map
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, "Error serializing response", http.StatusInternalServerError)
+		return
+	}
+}
